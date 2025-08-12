953c9bc9de585d1704db7397c37c4e1a8332ea1b
Rajesh Mondal <rajesh01749@gmail.com>
2025-08-12 16:52:59 +0600

    create HTML elements with JS and append to DOM

---
+++ 08. Tour of DOM (Document Object Model)/js/append.js
@@ -0,0 +1,40 @@
+const placeList = document.getElementById('places-list');
+const Li = document.createElement('li');
+Li.innerText = 'Italy';
+placeList.appendChild(Li);
+
+const mainContainer = document.getElementById('main-container');
+
+const section = document.createElement('section');
+const h1 = document.createElement('h1');
+h1.innerText = "My Food List";
+section.appendChild(h1);
+
+const ul = document.createElement('ul');
+const li1 = document.createElement('li');
+li1.innerText = 'Biriyani';
+ul.appendChild(li1);
+
+const li2 = document.createElement('li');
+li2.innerText = 'Pizza';
+ul.appendChild(li2);
+
+const li3 = document.createElement('li');
+li3.innerText = 'Fries';
+ul.appendChild(li3);
+
+section.appendChild(ul);
+
+mainContainer.appendChild(section);
+
+// set innerHTML directly
+const sectionDress = document.createElement('section');
+sectionDress.innerHTML = `
+<h1>My Dress Section</h1>
+<ul>
+    <li>Shirt</li>
+    <li>Jeans</li>
+    <li>T-shirt</li>
+</ul>
+`
+mainContainer.appendChild(sectionDress);
